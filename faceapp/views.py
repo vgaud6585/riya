2ca7f014a542eda05096493e787aaab60b255c03
Vishal gaud 9 <vgaud6585@gmail.com>
2026-02-13 10:29:55 +0530

    fews tax

--- faceapp/views.py
+++ faceapp/views.py
@@ -3,6 +3,9 @@ import os
 from django.shortcuts import render
 
 def show_gallery(request):
-	dt = {'images': images}
+	dt = {
+		#'images': images
+		
+	}
 	
 	return render(request, 'gallery.html', dt )
